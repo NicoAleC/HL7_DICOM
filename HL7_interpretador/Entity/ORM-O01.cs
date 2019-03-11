a463934e69f82c3613d2bd14d084d735deebf237
Nicolas Carrasco <chincholax@gmail.com>
2019-03-10 20:42:27 -0400

    first commit

---
+++ HL7_interpretador/Entity/ORM-O01.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL7_interpretador.Entity
+{
+    class ORM_O01
+    {
+        public string[] MSH { get; set; }
+        public string[] PID { get; set; }
+        public string[] ORC { get; set; }
+    }
+}
