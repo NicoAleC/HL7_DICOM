a4c1c2512f6905439ddd4d0452b620f9e858ab50
Nicolas Carrasco <chincholax@gmail.com>
2019-03-15 09:37:07 -0400

    entidades: agregados constructores a la clase Estudio

--- HL7_interpretador/Entity/Estudio.cs
+++ HL7_interpretador/Entity/Estudio.cs
@@ -8,5 +8,22 @@ namespace HL7_interpretador.Entity
 {
     class Estudio
     {
+        public Cliente Paciente { get; set; }
+        public string MedicoDeReferencia { get; set; }
+        public string EstudioRequerido { get; set; }
+
+        public Estudio()
+        {
+            this.Paciente = new Cliente();
+            this.MedicoDeReferencia = "";
+            this.EstudioRequerido = "";
+        }
+
+        public Estudio(Cliente paciente, string medico, string estudio)
+        {
+            this.Paciente = paciente;
+            this.MedicoDeReferencia = medico;
+            this.EstudioRequerido = estudio;
+        }
     }
 }
