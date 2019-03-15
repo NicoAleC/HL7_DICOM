576c00e086f4f449a52ebc00efba604336558fbb
Nicolas Carrasco <chincholax@gmail.com>
2019-03-15 09:30:37 -0400

    entidades: agregada la entidad Paciente

---
+++ HL7_interpretador/Entity/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL7_interpretador.Entity
+{
+    class Cliente
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Sexo { get; set; }
+        public double Peso { get; set; }
+        public string MedicoDeReferencia { get; set; }
+
+        public Cliente()
+        {
+            this.Id = "";
+            this.Nombre = "";
+            this.Sexo = "";
+            this.Peso = 0;
+            this.MedicoDeReferencia = "";
+        }
+
+        public Cliente(string id, string nombre, string sexo, double peso, string medico)
+        {
+            this.Id = id;
+            this.Nombre = nombre;
+            this.Sexo = sexo;
+            this.Peso = peso;
+            this.MedicoDeReferencia = medico;
+        }
+    }
+}
