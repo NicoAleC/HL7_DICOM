7620cbaf4b4274e92bf17178d7c49877ddaedc67
Paola Lizarraga <lizarragapk@gmail.com>
2019-03-16 16:56:29 -0400

    formularios: formulario solicitudes pendientes

---
+++ HL7_interpretador/formularios/solicitudes_pendientes.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace HL7_interpretador.formularios
+{
+    partial class solicitudes_pendientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // solicitudes_pendientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "solicitudes_pendientes";
+            this.Text = "solicitudes_pendientes";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
