a5442d170f5313f82155cb4e72c7ea2332c29673
Nicolas Carrasco <chincholax@gmail.com>
2019-03-24 11:00:18 -0400

    controladores: nuevos formularios agregados

---
+++ HL7_interpretador/formularios/Inicio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HL7_interpretador.Control;
+
+namespace HL7_interpretador.formularios
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Inicio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Servidor s = new Servidor();
+        }
+    }
+}
--- HL7_interpretador/formularios/lista_de_trabajo.Designer.cs
+++ HL7_interpretador/formularios/lista_de_trabajo.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(lista_de_trabajo));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.FECHA = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -49,9 +50,10 @@
             this.FECHA,
             this.MODALIDAD,
             this.ESTUDIO});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 21);
+            this.dataGridView1.Location = new System.Drawing.Point(16, 26);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(592, 408);
+            this.dataGridView1.Size = new System.Drawing.Size(789, 502);
             this.dataGridView1.TabIndex = 0;
             // 
             // ID
@@ -81,18 +83,20 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(642, 122);
+            this.label1.Location = new System.Drawing.Point(856, 150);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(123, 29);
+            this.label1.Size = new System.Drawing.Size(127, 29);
             this.label1.TabIndex = 1;
             this.label1.Text = "Filtrar por:";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(629, 169);
+            this.button1.Location = new System.Drawing.Point(839, 208);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(149, 36);
+            this.button1.Size = new System.Drawing.Size(199, 44);
             this.button1.TabIndex = 2;
             this.button1.Text = "Estudio";
             this.button1.UseVisualStyleBackColor = true;
@@ -100,9 +104,10 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(629, 232);
+            this.button2.Location = new System.Drawing.Point(839, 286);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(149, 36);
+            this.button2.Size = new System.Drawing.Size(199, 44);
             this.button2.TabIndex = 3;
             this.button2.Text = "Modalidad";
             this.button2.UseVisualStyleBackColor = true;
@@ -110,9 +115,10 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(629, 294);
+            this.button3.Location = new System.Drawing.Point(839, 362);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(149, 36);
+            this.button3.Size = new System.Drawing.Size(199, 44);
             this.button3.TabIndex = 4;
             this.button3.Text = "Paciente";
             this.button3.UseVisualStyleBackColor = true;
@@ -121,24 +127,28 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(647, 54);
+            this.button4.Location = new System.Drawing.Point(863, 66);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(106, 30);
+            this.button4.Size = new System.Drawing.Size(141, 37);
             this.button4.TabIndex = 5;
             this.button4.Text = "Ordenar";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // lista_de_trabajo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "lista_de_trabajo";
             this.Text = "lista_de_trabajo";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
